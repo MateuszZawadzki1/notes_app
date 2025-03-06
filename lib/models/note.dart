ae58c7df6c2f841a1c4b7a7a0d3cd95f49ed1ac5
Mateusz Zawadzki <matizawadzki@interia.pl>
2025-03-06 23:24:33 +0100

    feat: implement Note model JSON conversion

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,11 +1,28 @@
 import 'package:uuid/uuid.dart';
 
-const uuid = Uuid();
+// const uuid = Uuid();
 
 class Note {
-  Note({required this.user, required this.text}) : id = uuid.v4();
+  Note({this.id, required this.user, required this.text});
 
-  final String id;
+  final int? id;
   final String user;
   final String text;
+
+  // JSON -> Note
+  factory Note.fromJson(Map<String, dynamic> json) {
+    return Note(
+      id: json['id'],
+      user: json['user'],
+      text: json['text'],
+    );
+  }
+
+  // Note -> JSON
+  Map<String, dynamic> toJson() {
+    return {
+      "user": user,
+      "text": text,
+    };
+  }
 }
