7a57024c6b49225d90977406f4459909467eb00f
Mateusz Zawadzki <matizawadzki@interia.pl>
2025-03-12 20:04:24 +0100

    None

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/services/auth_service.dart';
+import 'package:notes_app/services/supabase_service.dart';
 import 'package:notes_app/widgets/login_screen.dart';
 import 'package:notes_app/widgets/notes.dart';
 
 void main() {
+  final authService = AuthService();
+  final supabaseService = SupabaseService(authService);
+
   runApp(const MaterialApp(home: LoginScreen() //Notes(),
       // TODO: Theming TextButton, TitleFont
       ));
